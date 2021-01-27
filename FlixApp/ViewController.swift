b76611914e752985776022b4e9cb694f5a8e3d46
Michael Winder <5003376893@student.csn.edu>
2021-01-26 19:24:29 -0800

    Initial Commit

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by Michael Winder on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
