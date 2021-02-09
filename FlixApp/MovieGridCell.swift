949b6a1c8001173f3f16032183f6ebb797da4879
Michael Winder <5003376893@student.csn.edu>
2021-02-08 20:16:09 -0800

    Unit 2 Part 2 commit

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Michael Winder on 2/3/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell
+{
+    @IBOutlet weak var posterView: UIImageView!
+}
